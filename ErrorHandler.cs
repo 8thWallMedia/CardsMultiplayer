5dbfd85404243d8d1bc9414e936c604cfe981524
Cameron Smith <riley805@windstream.net>
2013-05-25 23:28:57 -0400

    Error Handler login responses added

--- ErrorHandler.cs
+++ ErrorHandler.cs
@@ -20,6 +20,24 @@ namespace CardsMultiplayer
     */
     class ErrorHandler
     {
-
+        public static const int ERROR_INVALID_CREDENTIALS = 100;
+        public static const int ERROR_INVALID_LOGIN_OFFLINE = 101;
+        public static const int ERROR_INVALID_LOGIN_RESPONSE = 102;
+        public static const int ERROR_LOGINSERVER_TIMEOUT = 103;
+        public static const int ERROR_ACCOUNT_DISABLE = 104;
+        public static const int ERROR_HOST_BANNED = 105;
+        public static string GetErrorMessage(int ErrorID)
+        {
+            switch (ErrorID)
+            {
+                case ERROR_INVALID_CREDENTIALS: return "The username/password was incorrect.";
+                case ERROR_INVALID_LOGIN_OFFLINE: return "The login server is offline.";
+                case ERROR_INVALID_LOGIN_RESPONSE: return "The server returned an unexpected response.";
+                case ERROR_LOGINSERVER_TIMEOUT: return "The login request has timed out.";
+                case ERROR_ACCOUNT_DISABLE: return "Your account has been disabled!";
+                case ERROR_HOST_BANNED: return "Your host address was rejected by the server.";
+            }
+            return "Unhandled Exception.";
+        }
     }
 }
